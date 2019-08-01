adf4bf7b18e6b10be8e61ab34ac7dbd5c512b3fc
koala <670677781@qq.com>
2019-08-01 23:53:52 +0800

    初始

--- nacos/src/main/java/com/NacosApplication.java
+++ nacos/src/main/java/com/NacosApplication.java
@@ -1,13 +1,25 @@
 package com;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
 
-@SpringCloudApplication
+import java.util.Properties;
+
 public class NacosApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NacosApplication.class, args);
     }
 
+    /**
+     * @return 
+     */
+    public static boolean isOSLinux() {
+        Properties prop = System.getProperties();
+        String os = prop.getProperty("os.name");
+        if (os != null && os.toLowerCase().contains("linux")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
